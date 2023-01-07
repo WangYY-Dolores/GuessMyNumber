a697897b16fffce49a6afa897e1e432fbdd3fec7
王媛原 <1299795278@qq.com>
2023-01-07 16:32:18 -0500

    refactored a litte bit

--- script.js
+++ script.js
@@ -2,42 +2,42 @@
 
 let randomNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
+
+const displayMessage = function(message){
+    document.querySelector('.message').textContent = message;
+}
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     if(!guess){
-        document.querySelector('.message').textContent = 'No number input!';
+        displayMessage('No number input!');
     } else if ( guess === randomNumber){
-        document.querySelector('.message').textContent = 'Correct Number!';
+        displayMessage('Correct Number!');
         document.querySelector('.number').textContent = randomNumber;
 
         document.querySelector('body').style.backgroundColor = 'pink';
         document.querySelector('.number').style.width = '30rem'
-    } else if (guess > randomNumber){
+
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+    } else if (guess !== randomNumber){
         if(score > 1){
-            document.querySelector('.message').textContent = 'Too high!';
+            displayMessage(guess > randomNumber ? 'Too high!' : 'Too low!');
             score--;
             document.querySelector('.score').textContent = score;
         } else {
-            document.querySelector('.message').textContent = 'You lose!';
+            displayMessage('You lose!');
         }
-        
-    } else if (guess < randomNumber){
-        if(score > 1){
-            document.querySelector('.message').textContent = 'Too Low!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = 'You lose!';
-        }
-        
     }
 })
 
 document.querySelector('.again').addEventListener('click', function() {
     score = 20;
     randomNumber = Math.trunc(Math.random() * 20) + 1;
-    document.querySelector('.message').textContent = 'Start guessing...';
+    displayMessage('Start guessing...');
     document.querySelector('.score').textContent = score;
     document.querySelector('.number').textContent = '?';
     document.querySelector('.guess').value = '';
